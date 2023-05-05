37e969513c9c3d5a5eb3ef65c48dd0fd23eb7fa2
William <124058487+WilliamW1234567@users.noreply.github.com>
2023-05-05 08:39:09 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,2 +1,23 @@
-class Move:
-    def __init__(attack, defend, heal):
+class move:
+    def __init__(self, name):
+        self.name=name 
+
+class attack(move):
+    def __init__(self, name, damage):
+        super().__init__(name)
+        self.damage = damage
+    def __str__(self):
+        return f"{self.name}, {self.damage}"
+
+class defend(move):
+    def __init__(self, name, block):
+        super().__init__(name)
+        self.block = block
+        return f"{self.name}, {self.block}"
+
+class heal(move):
+    def __init__(self, name, eat):
+        super().__init__(name)
+        self.eat = eat
+        return f"{self.name}, {self.eat}"
+        
