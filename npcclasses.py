20a70880373d878f364c2f8723a3bee1fdf2bb2c
William <124058487+WilliamW1234567@users.noreply.github.com>
2023-05-08 08:39:41 -0400

    Update npcclasses.py

---
+++ npcclasses.py
@@ -0,0 +1,9 @@
+class stats:
+    def __init__(self, attack, health):
+        self.attack = attack
+        self.health = health
+class user(stats):
+    def __init__(self, attack, health):
+        self.attack = attack
+        self.health = health
+
