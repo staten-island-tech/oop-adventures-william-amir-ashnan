b8cadfe8dc9f29cf5e0e7c0c764cec8490d080e4
William <124058487+WilliamW1234567@users.noreply.github.com>
2023-05-11 08:14:28 -0400

    Update npcclasses.py

--- npcclasses.py
+++ npcclasses.py
@@ -1,10 +1,18 @@
-class stats:
+class player:
+    def __init__(self, name):
+        self.name = name
+        self.attack = 500
+        self.health = 1000
+
+class NPC(player):
     def __init__(self, name, attack, health):
+        super().__init__(name)
         self.name = name
         self.attack = attack
         self.health = health
+        return f"{self.name}, {self.attack}, {self.health}"
 
-DoctorOctopus = stats("Doctor Octopus", "15", "125" )
-Electro = stats("Electro", "20", "150")
-Mysterio = stats("Mysterio", "25", "175")
-GreenGoblin = stats("Green Goblin", "40", "175")
+DoctorOctopus = NPC("Doctor Octopus", "15", "125" )
+Electro = NPC("Electro", "20", "150")
+Mysterio = NPC("Mysterio", "25", "175")
+GreenGoblin = NPC("Green Goblin", "40", "175")
