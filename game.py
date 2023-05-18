190a814b3fc0b6aff864d459865fbb8f819055f6
William <124058487+WilliamW1234567@users.noreply.github.com>
2023-05-18 08:39:21 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
-
+import random
 class Character:
-    def __init__(self, name, attack, health):
+    def __init__(self, name, attack, defend, health):
         self.name = name
         self.attack = attack
         self.health = health
-        
+        self.defend = defend
+
     def alive(self):
         return self.health > 0
         
@@ -13,11 +14,11 @@ class Character:
         print(f"{self.name} attacked {enemy.name} for {self.attack} damage.")
         
 class Hero(Character):
-    def __init__(self, name, attack, health, heal):
-        super().__init__(name, attack, health)
+    def __init__(self, name, attack, health, block, heal):
+        super().__init__(name, attack, block, health)
         self.heal = heal
         
-    def use_pizza(self):
+    def pizza(self):
         self.health += self.heal
         print(f"{self.name} ate a slice of pizza and regained {self.heal} health.")
         
@@ -31,19 +32,29 @@ class Enemy(Character):
 
 class DoctorOctopus(Enemy):
     def __init__(self):
-        super().__init__("Doctor Octopus", 15, 125)
+        super().__init__("Doctor Octopus", random.randint(10,20), 125)
         
 class Electro(Enemy):
     def __init__(self):
-        super().__init__("Electro", 20, 150)
+        super().__init__("Electro", random.randint(20,25), 150)
         
 class Mysterio(Enemy):
     def __init__(self):
-        super().__init__("Mysterio", 25, 175)
+        super().__init__("Mysterio", random.randint(25,30), 175)
 
 class GreenGoblin(Enemy):
     def __init__(self):
-        super().__init__("Green Goblin", 40, 200)
+        super().__init__("Green Goblin", random.randint(30,40), 200)
+
+class Game:
+    def __init__(self):
+        self.game_over = False
+        self.round = 0
+
+    def new_round(self):
+        self.round += 1
+        print(f"\n***   Round: {self.round}   ***\n")  
+
 
 def prefight(hero, enemy): 
     print(f"{hero.name} vs {enemy.name}") 
@@ -52,12 +63,10 @@ def prefight(hero, enemy):
 
 def fight(hero, enemy):
     while hero.alive() and enemy.alive():
-        # Hero attacks first
         hero.attack_enemy(enemy)
         if not enemy.alive():
             break
             
-        # Enemy attacks
         enemy.attack_enemy(hero)
         if not hero.alive():
             break
@@ -69,8 +78,10 @@ def fight(hero, enemy):
         print(f"{hero.name} was defeated by {enemy.name}. Game over!")
         return False
 
+games = Game()
+
 def main():
-    hero = Hero("Spider-Man", 30, 150, 25)
+    hero = Hero("Spider-Man", 30, 10000, 25)
     enemies = [DoctorOctopus(), Electro(), Mysterio(), GreenGoblin()]
     
     print("Welcome to Queens! Your mission is to defeat all the enemies and save the city.")
@@ -79,7 +90,10 @@ def main():
         print(f"You have encountered {enemy.name}. Get ready to fight!")
         answer = input("Are you ready to accept the challenge? (yes or no) ")
         if answer.lower() == "yes":
+            games.new_round()
+            prefight(hero, enemy)
             fight(hero, enemy)
+            print(f"Your health = {hero.health} {enemy.name} health = {enemy.health}")
             if not hero.alive():
                 break
         else:
@@ -87,7 +101,7 @@ def main():
             break
             
     if hero.alive():
-        print("Congratulations! You have saved Queens from the Green Goblin's reign of terror!")
+        print("Congratulations! You have saved Queens from the Villians reign of terror!")
 if __name__ == "__main__":
     main()
 
