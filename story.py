0baba6a55c476ec074336736947e5f7c5c363fcb
William <124058487+WilliamW1234567@users.noreply.github.com>
2023-05-26 08:30:12 -0400

    Delete story.py

--- story.py
+++
@@ -1,7 +0,0 @@
-start = input("Welcome to SpiderMan Adventures, do you want to play? Y/N")
-if start == ("Y"):
-    print("Welcome to Queens, you must protect the city and help all the citizens live in peace and harmony. As your journey continues you will act as Spider-Man and will have to face enemies from different stand points. Remember you have weapons and an occasional powerup to fight the villains. If your health is low, eat a slice of Joe’s pizza to regain 25 health. Good Luck, the citizens of Queens are counting on you.")
-if start == ("N"):
-    print("Hope you change your mind later")
-
-
