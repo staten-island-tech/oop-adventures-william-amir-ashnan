1200e15fe065975ba0a77010a8fe34d8caebb2f1
William <124058487+WilliamW1234567@users.noreply.github.com>
2023-05-26 08:25:42 -0400

    e

--- npcclasses.py
+++ npcclasses.py
@@ -1,21 +1,57 @@
-class player:
-    def __init__(self, name):
-        self.name = name
-        self.attack = 500
-        self.health = 1000
-
-class NPC(player):
+import random
+class Character:
     def __init__(self, name, attack, health):
-        super().__init__(name)
         self.name = name
         self.attack = attack
         self.health = health
-    def __str__(self):
-        return f"{self.name}, {self.attack}, {self.health}"
 
-DoctorOctopus = NPC("Doctor Octopus", "15", "125" )
-Electro = NPC("Electro", "20", "150")
-Mysterio = NPC("Mysterio", "25", "175")
-GreenGoblin = NPC("Green Goblin", "40", "175")
+    def block(self, enemy):
+        self.health += enemy.attack
+        print(f"{self.name} blocked {enemy.name} for {enemy.attack} damage")
+    
+    def alive(self):
+        return self.health > 0
+        
+    def attack_enemy(self, enemy):
+        enemy.health -= self.attack
+        print(f"{self.name} attacked {enemy.name} for {self.attack} damage.")
+        
+class Hero(Character):
+    def __init__(self, name, attack, health, heal):
+        super().__init__(name, attack, health)
+        self.heal = heal
+    
+    def quit(self):
+        self.health -= self.health
 
-print(DoctorOctopus.health)
+    def pizza(self):
+        self.health += self.heal
+        print(f"{self.name} ate a slice of pizza and regained {self.heal} health.")
+        
+class Enemy(Character):
+    pass
+
+class DoctorOctopus(Enemy):
+    def __init__(self):
+        super().__init__("Doctor Octopus", random.randint(10,20), 125)
+        
+class Electro(Enemy):
+    def __init__(self):
+        super().__init__("Electro", random.randint(20,25),150)
+        
+class Mysterio(Enemy):
+    def __init__(self):
+        super().__init__("Mysterio", random.randint(25,30), 175)
+
+class GreenGoblin(Enemy):
+    def __init__(self):
+        super().__init__("Green Goblin", random.randint(30,40), 200)
+
+class Game:
+    def __init__(self):
+        self.game_over = False
+        self.challenge = 0
+
+    def new_round(self):
+        self.challenge += 1
+        print(f"\n***   Enemy: {self.challenge}   ***\n")  
