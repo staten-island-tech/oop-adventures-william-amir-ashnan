401c3545c45897790dd2a61010562909c9670efc
amirjon-k <amirjonk1982@gmail.com>
2023-06-06 08:10:31 -0400

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ else:
      print('You have lost the game')
      exit() 
 
-answer == input('Congrats on defeating Doctor Octopus! Time for the next opponent, Electro, he is slightly harder than Doctor Octopus. He has a health of 150 and attack damage of 20. Are you ready to accept your second challenge?')
+answer == input('Congrats on defeating Doctor Octopus! Time for the next opponent, Electro, who is slightly harder than Doctor Octopus and has a health of 150 and attack damage of 20. Are you ready to accept your second challenge?')
 if answer == 'yes':
     print('Time to fight!')
 
@@ -17,12 +17,12 @@ else:
      print('You have lost the game')
      exit() 
 
-print('Congrats on defeating Electro! Time for the next opponent, Mysterio, he is slightly harder than Electro. He has a health of 175 and attack damage of 25.')
+print('Congrats on defeating Electro! Time for the next opponent, Mysterio, who is slightly harder than Electro. He has a health of 175 and attack damage of 25.')
 
 answer == input('Are you ready to accept your third challenge?')
 if answer == 'yes':
     print('Time to fight!')
 
 else:
-     print('You have lost the game')
+     print('You have lost the game!')
      exit() 
