17d956216675aca23dc0bc6e6015d087a080fb5c
amirjon-k <amirjonk1982@gmail.com>
2023-06-07 08:40:20 -0400

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,5 +1,11 @@
-answer=input("Do you want to play the Spider-Man game?")
-if answer =="Yes":
-    print("Welcome to Queens, you must protect the city and help all the citizens live in peace and harmony.As your journey continues you will act as Spider-Man and will have to face enemies from different stand points. Remember you have weapons and an occasional powerup to fight the villains. If your health is low, eat a slice of Joe’s pizza to regain 25 health. Good Luck, the citizens of Queens are counting on you.")
-if answer =="No":
-    print("Bye")
+from game import main, Tutorial
+answer=input("Do you want to play the Spider-Man game tutorial? yes/no")
+if answer.lower() == "yes":
+    print("Welcome to the tutorial, in this game you can attack, defend, or heal!")
+    Tutorial()
+    print("You have finished he tutorial, now  you can start the main game")
+    print("Welcome to Queens, you must protect the city and help all the citizens live in peace and harmony.As your journey continues you will act as Spider-Man and will have to face enemies from different stand points. Remember you have weapons and an occasional powerup to fight the villains. If your health is low, eat a slice of Joe’s pizza to regain health. Good Luck, the citizens of Queens are counting on you.")
+    main()
+if answer.lower() == "no":
+    print("Welcome to Queens, you must protect the city and help all the citizens live in peace and harmony.As your journey continues you will act as Spider-Man and will have to face enemies from different stand points. Remember you have weapons and an occasional powerup to fight the villains. If your health is low, eat a slice of Joe’s pizza to regain health. Good Luck, the citizens of Queens are counting on you.")
+    main()
